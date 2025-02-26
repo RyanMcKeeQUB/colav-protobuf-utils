b58cb712ef574a7c75f3a94cee3e74311f178c4b
Ryan McKee <r.mckee@qub.ac.uk>
2025-02-26 14:09:47 +0000

    UPDATE: Intial release for colav-protobuf-utils

--- tests/protobuf_generator/test_agent_update.py
+++ tests/protobuf_generator/test_agent_update.py
@@ -3,32 +3,44 @@ from colav_protobuf.examples import agent_update
 import pytest
 
 
+def get_cartesian_position(state):
+    return [state.pose.position.x, state.pose.position.y, state.pose.position.z]
+
+
+def get_quaternion_orientation(state):
+    return [
+        state.pose.orientation.x,
+        state.pose.orientation.y,
+        state.pose.orientation.z,
+        state.pose.orientation.w,
+    ]
+
+
 def test_gen_agent_update():
+    """pytest assertion tests for generation of protobuf agent update"""
     proto_utils_agent_update = gen_agent_update(
         mission_tag=agent_update.mission_tag,
         agent_tag=agent_update.agent_tag,
-        cartesian_position=[
-            agent_update.state.pose.position.x,
-            agent_update.state.pose.position.y,
-            agent_update.state.pose.position.z,
-        ],
-        quaternium_orientation=[
-            agent_update.state.pose.orientation.x,
-            agent_update.state.pose.orientation.y,
-            agent_update.state.pose.orientation.z,
-            agent_update.state.pose.orientation.w,
-        ],
+        cartesian_position=get_cartesian_position(agent_update.state),
+        quaternium_orientation=get_quaternion_orientation(agent_update.state),
         velocity=agent_update.state.velocity,
         yaw_rate=agent_update.state.yaw_rate,
         acceleration=agent_update.state.acceleration,
         timestamp=agent_update.timestamp,
     )
-    print(proto_utils_agent_update)
 
-
-def main():
-    test_gen_agent_update()
-
-
-if __name__ == "__main__":
-    main()
+    assert proto_utils_agent_update.mission_tag == agent_update.mission_tag
+    assert proto_utils_agent_update.agent_tag == agent_update.agent_tag
+    assert (
+        proto_utils_agent_update.state.pose.position == agent_update.state.pose.position
+    )
+    assert (
+        proto_utils_agent_update.state.pose.orientation
+        == agent_update.state.pose.orientation
+    )
+    assert proto_utils_agent_update.state.velocity == agent_update.state.velocity
+    assert proto_utils_agent_update.state.yaw_rate == agent_update.state.yaw_rate
+    assert (
+        proto_utils_agent_update.state.acceleration == agent_update.state.acceleration
+    )
+    assert proto_utils_agent_update.timestamp == agent_update.timestamp
